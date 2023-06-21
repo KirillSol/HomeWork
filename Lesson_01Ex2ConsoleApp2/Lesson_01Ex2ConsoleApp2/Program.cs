0c73514ba8a65c84fbe04fdf02428427447d6770
KirillSol <kirill2002.sob@gmail.com>
2023-06-21 18:43:02 +0300

    Добавьте файлы проекта.

---
+++ Lesson_01Ex2ConsoleApp2/Lesson_01Ex2ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Lesson_01Ex2ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Я переношу строку после выполнения своей функции, а ты?");
+            Console.Write("А я нет, смотри!");
+            Console.WriteLine();
+            Console.Write("Hello, ");
+            Console.Write("World ");
+            Console.Write("!!!");
+
+            Console.ReadLine();
+        }
+    }
+}
